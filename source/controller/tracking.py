a1962a5202eeb0b394e56734d775fd6b9c9e124b
Jonathan Perry-Houts <jperryh2@uoregon.edu>
2015-10-14 11:48:47 -0700

    Added Piwik tracking.

---
+++ source/controller/tracking.py
@@ -0,0 +1,20 @@
+piwik = '''
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//www.invertedearth.net/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//www.invertedearth.net/piwik/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+'''
+
+def piwik_code():
+    return piwik
