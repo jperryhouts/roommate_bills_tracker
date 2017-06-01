a28b974192dfdc1259adae3051720509c1b3c16b
Jonathan Perry-Houts <jperryh2@uoregon.edu>
2017-05-31 17:54:58 -0700

    Add license message to source files

--- source/view/page_elements.py
+++ source/view/page_elements.py
@@ -1,4 +1,20 @@
 # -*- coding: UTF-8 -*-
+"""
+This file is part of Bill-Tracker.
+
+Bill-Tracker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bill-Tracker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bill-Tracker.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import StringIO
 from decimal import Decimal as Dec
